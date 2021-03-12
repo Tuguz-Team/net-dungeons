b0c01ab80f66a5978f20d4b61d863c5c225a8670
tuguzT <timurka.tugushev@gmail.com>
2021-03-12 23:21:26 +0300

    Badlogic JPEG image was removed, added Java 8 support in Android project, added basic transitions between fragments, NetDungeonsGame class was simplified

--- android/src/com/tuguzteam/netdungeons/MainMenuFragment.kt
+++ android/src/com/tuguzteam/netdungeons/MainMenuFragment.kt
@@ -5,11 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.button.MaterialButton
 
 class MainMenuFragment : Fragment() {
+    private lateinit var materialButton: MaterialButton
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main_menu, container, false)
+        val view = inflater.inflate(R.layout.fragment_main_menu, container, false)
+        materialButton = view.findViewById(R.id.fragment_main_menu_button)
+        materialButton.setOnClickListener {
+            parentFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<GameFragment>(R.id.activity_main_fragment_container)
+                addToBackStack(null)
+            }
+        }
+        return view
     }
 }
--- core/src/com/tuguzteam/netdungeons/NetDungeonsGame.kt
+++ core/src/com/tuguzteam/netdungeons/NetDungeonsGame.kt
@@ -2,29 +2,11 @@ package com.tuguzteam.netdungeons
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ktx.app.KtxApplicationAdapter
 
 class NetDungeonsGame : KtxApplicationAdapter {
-    private lateinit var img: Texture
-    private lateinit var batch: SpriteBatch
-
-    override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
-    }
-
     override fun render() {
         Gdx.gl.glClearColor(0f, 1f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
-    }
-
-    override fun dispose() {
-        batch.dispose()
-        img.dispose()
     }
 }
